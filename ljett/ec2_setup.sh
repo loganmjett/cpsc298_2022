54ec3d47be1e46bbe83f7f788e98c065408dbd1c
Logan Jett <ljett@chapman.edu>
2022-03-15 11:57:20 -0700

    altered script to include sed

--- ljett/ec2_setup.sh
+++ ljett/ec2_setup.sh
@@ -6,5 +6,8 @@ mkdir /ljcode
 cd /ljcode
 git clone --branch main https://github.com/loganmjett/cpsc298_2022.git
 cp cpsc298_2022/ljett/html/index.html /var/www/html/index.html
+export da=`date`
+env | grep da
+sed -i "s/REPLACE_STRING/${da}/g" index.html
 service httpd start
 chkconfig httpd on
